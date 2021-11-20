d1d46e854792b9066a1e935c26616975e3eaba0d
tony hennequin <thennequin@sii.fr>
2021-11-20 16:48:54 +0100

    IC-2020.3.3 <thennequin@FRRNS-L00356 Create debugger.xml, lombok-plugin.xml, colors.scheme.xml, updates.xml, smart.tomcat.xml, keymap.xml, editor.xml, projectView.xml, Default.xml, ide.general.xml, project.default.xml, baseRefactoring.xml, studioFlags.xml, Java.xml, acceptedLanguageLevels.xml, External Tools.xml, asciidoc.xml, wsl.distributions.xml, pluginAdvertiser.xml, _@user_Darcula.icls, KeyPromoterXSettings.xml, markdown.xml, path.macros.xml, sonarlint.xml, toStringTemplates.xml, laf.xml, ui.lnf.xml, grazie_global.xml, usageView.xml, memory.view.xml, Windows copy.xml, ide-features-trainer.xml, remote-servers.xml, Default.xml, javaeeExternalResources.xml, GrepConsole.xml, log_highlighting.xml, jdk.table.xml, JUnit4 Test Class.java, notifications.xml, filetypes.xml, vcs.xml, terminal.xml, file.template.settings.xml, templates.xml, find.xml, diff.xml

---
+++ fileTemplates/code/JUnit4 Test Class.java
@@ -0,0 +1,14 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+#parse("File Header.java")
+@RunWith(MockitoJUnitRunner.class)
+public class ${NAME} {
+  ${BODY}
+}
